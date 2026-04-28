ee29597f00da3f0ac37ad4566b7e3fa931a99715
Tangi-does-not-code <Theonlypedrochaves@gmail.com>
2026-04-28 11:53:27 +0000

    Update DebloatWin11.ps1

---
+++ DebloatWin11.ps1
@@ -0,0 +1,82 @@
+
+#remover os arquivos temp
+
+Remove-Item -Path "$env:TEMP\*" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Windows\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue
+
+#destiva a telemetria
+Set-Service "DiagTrack" -StartupType Disabled -ErrorAction SilentlyContinue
+Stop-Service "DiagTrack" -Force -ErrorAction SilentlyContinue
+
+
+#Tira os apps da xbox
+Get-AppxPackage *xbox* | Remove-AppxPackage -ErrorAction SilentlyContinue
+Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -like "*xbox*"} | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+
+
+#Tira todos os servicos da xbox (literalmente tudo, entao se voce for ultilizar esse codigo aqui com o gamepass ou algo do tipo, nao vai dar certo)
+Stop-Service "XblAuthManager" -Force -ErrorAction SilentlyContinue
+Stop-Service "XblGameSave" -Force -ErrorAction SilentlyContinue
+Stop-Service "XboxNetApiSvc" -Force -ErrorAction SilentlyContinue
+Stop-Service "XboxGipSvc" -Force -ErrorAction SilentlyContinue
+
+Set-Service "XblAuthManager" -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service "XblGameSave" -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service "XboxNetApiSvc" -StartupType Disabled -ErrorAction SilentlyContinue
+Set-Service "XboxGipSvc" -StartupType Disabled -ErrorAction SilentlyContinue
+
+
+#exclui a cortana
+Get-AppxPackage *cortana* | Remove-AppxPackage -ErrorAction SilentlyContinue
+Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -like "*cortana*"} | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+
+#remobe o bingweather (o wiget de clima)
+Get-AppxPackage *bingweather* | Remove-AppxPackage -ErrorAction SilentlyContinue
+Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -like "*bingweather*"} | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+
+#remove o news (o wiget de noticias)
+Get-AppxPackage *news* | Remove-AppxPackage -ErrorAction SilentlyContinue
+Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -like "*news*"} | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+
+#remove dicas e sugestoes
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SubscribedContent-338389Enabled" -Value 0 -ErrorAction SilentlyContinue
+
+#ativa automaticamente o modo desempenho
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "VisualFXSetting" -Value 2 -ErrorAction SilentlyContinue
+
+#ativa o modo de alto desempenho do windows (vai puxar mais energia, mas vai melhorar o desempenho)
+powercfg -setactive SCHEME_MIN
+
+#Desativa os apps em segundo plano
+Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" -ErrorAction SilentlyContinue | ForEach-Object {
+    Set-ItemProperty -Path $_.PSPath -Name "Disabled" -Value 1 -ErrorAction SilentlyContinue
+}
+
+#tira o copilot completamente
+New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows\WindowsCopilot" -Force -ErrorAction SilentlyContinue
+Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\WindowsCopilot" -Name "TurnOffWindowsCopilot" -Value 1 -Type DWord -ErrorAction SilentlyContinue
+
+
+
+#Wallpaper preto para mais performance idle
+Set-ItemProperty -Path "HKCU:\Control Panel\Colors" -Name "Background" -Value "0 0 0" -ErrorAction SilentlyContinue
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "Wallpaper" -Value "" -ErrorAction SilentlyContinue
+
+#aplica as mudancas visuais
+rundll32.exe user32.dll,UpdatePerUserSystemParameters
+
+#reinicia o explorer
+Stop-Process -Name explorer -Force -ErrorAction SilentlyContinue
+Start-Process explorer
+
+#checa se tem algum update no Windows e se tiver atualiza
+Install-PackageProvider -Name NuGet -Force -ErrorAction SilentlyContinue
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted -ErrorAction SilentlyContinue
+Install-Module PSWindowsUpdate -Force -ErrorAction SilentlyContinue
+
+Import-Module PSWindowsUpdate -ErrorAction SilentlyContinue
+Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot -ErrorAction SilentlyContinue
+
+
+#ai ele reinicia.
+Restart-Computer -Force
